0cb48b2b9a74d66c616219455eacae6e82fbcc62
Ryan Yang <javathon@gmail.com>
2019-10-25 02:52:39 -0700

    added description to boba watch will push this preliminary verison for now

--- src/components/BobaWatch.js
+++ src/components/BobaWatch.js
@@ -5,8 +5,21 @@ const BobaWatch = () => {
     return (
         <div className="bwatch--wrapper">
             <div className="bwatch--description">
-                <div className="bwatch-title">
-                    boba watch
+                <div className="bwatch-title--holder">
+                    <div className="bwatch-title--logo"></div>
+                    <div className="bwatch-title">
+                        boba watch
+                    </div>
+                </div>
+                <div className="bwatch-about">
+                    <span> Founder and lead developer of Boba Watch. </span>
+                    Boba watch is responsive PWA that helps users track, finance, 
+                    and control their milk tea drinking habits. I developed 
+                    Boba Watch using ReactJS and utilizing service workers 
+                    for offline caching of the Firebase backend.
+                </div>
+                <div className="bwatch-about-after">
+
                 </div>
             </div>
             <div className="bwatch--device">
