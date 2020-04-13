c250cb6de72b060993f70e5cbab4b26313e0f64f
Ryan Yang <ryanqyang@gmail.com>
2020-04-12 20:39:14 -0700

    added interactive social media links

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Sidebar.scss';
-import Icons from '../Icons/Icons';
+import gmailIcon from '../../assets/icons/gmail.svg';
+import twitterIcon from '../../assets/icons/twitter.svg';
+import githubIcon from '../../assets/icons/github.svg';
+import instagramIcon from '../../assets/icons/instagram.svg';
+import {ReactComponent as SocialTriggerIcon} from '../../assets/icons/social_trigger.svg';
+
+const iconOrder = [
+    {
+        'img': githubIcon,
+        'type': 'github',
+        'link': 'https://github.com/ryqndev'
+    },
+    {
+        'img': twitterIcon,
+        'type': 'twitter',
+        'link': 'https://twitter.com/ryqndev'
+    },
+    {
+        'img': gmailIcon,
+        'type': 'gmail',
+        'link': 'mailto:rqyang@uci.edu'
+    },
+    {
+        'img': instagramIcon,
+        'type': 'instagram',
+        'link': 'https://instagram.com/lotsofsbux'
+    }
+];
+
+const Icon = ({data}) => {
+    return (
+        <div className="icon-holder">
+            <a
+                className="icon"
+                style={{backgroundImage: `url('${data.img}')`}}
+                aria-label={data.type}
+                href={data.link}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+            </a>
+        </div>
+    );
+}
 
 const Sidebar = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <aside className="s-w">
-            <div className="s-c">
-                <div className="s-line top"></div>
-                <div className="social">
-                    <Icons />
-                </div>
-                <div className="s-line bottom"></div>
-            </div>
-        </aside>
+        <div className={`social-trigger ${open ? "open" : ""}`} onClick={() => {setOpen(!open)}}>
+            <SocialTriggerIcon />
+            <aside className="social-popup">
+                {iconOrder.map(social => <Icon data={social}/>)}
+            </aside>
+        </div>
     )
 }
 
