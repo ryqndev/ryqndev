f759317cfbb0387e73510d66acb0912cdcb904eb
Ryan Yang <ryanqyang@gmail.com>
2020-10-14 09:32:10 -0700

    Updates

---
+++ precache-manifest.2611e88a33d2e814a72dacbdaaf611f3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd901d6453f60fea58badff00806ad88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd196607059aa283ec9c",
+    "url": "/static/css/main.a92fd9e0.chunk.css"
+  },
+  {
+    "revision": "2f06d3575a01c9e34535",
+    "url": "/static/js/2.ef50834b.chunk.js"
+  },
+  {
+    "revision": "21fc3923ea7a4029a8b6bd1cebe0cd2b",
+    "url": "/static/js/2.ef50834b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd196607059aa283ec9c",
+    "url": "/static/js/main.a6713f59.chunk.js"
+  },
+  {
+    "revision": "6cfe754d39b7f802f71e",
+    "url": "/static/js/runtime-main.3f68c55c.js"
+  },
+  {
+    "revision": "2cc7ab6df879d8a7f718325bc5b71c90",
+    "url": "/static/media/Hyperwave-Two.2cc7ab6d.woff"
+  },
+  {
+    "revision": "6b78c7ec468eb0e13c6c5c4c39203986",
+    "url": "/static/media/Poppins-ExtraBold.6b78c7ec.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "ebbe71e813de6561db35125ce23bc393",
+    "url": "/static/media/arrow.ebbe71e8.svg"
+  },
+  {
+    "revision": "86dab07ca1480b7f500b0988f8e82241",
+    "url": "/static/media/boba_watch.86dab07c.png"
+  },
+  {
+    "revision": "8f62048c868ad6465b49ea17e44f4ac1",
+    "url": "/static/media/resume.8f62048c.pdf"
+  },
+  {
+    "revision": "778001d44ec882c7953edc499086e062",
+    "url": "/static/media/splash-min.778001d4.png"
+  },
+  {
+    "revision": "3dc93cf70b6b0a957ce92070dcba5e32",
+    "url": "/static/media/webdev.3dc93cf7.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9251f5cac16593b7154b6ce4aa49eb04.js"
+  "/precache-manifest.2611e88a33d2e814a72dacbdaaf611f3.js"
 );
 
 self.addEventListener('message', (event) => {
