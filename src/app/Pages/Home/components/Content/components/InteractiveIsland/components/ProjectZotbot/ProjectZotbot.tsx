f355f1c6fdf23bd798ed7242b4f8a6da678f2802
Ryan Yang <ryanqyang@gmail.com>
2025-01-15 17:50:14 -0800

    lowered altitude to prevent falling off

--- src/app/Pages/Home/components/Content/components/InteractiveIsland/components/ProjectZotbot/ProjectZotbot.tsx
+++ src/app/Pages/Home/components/Content/components/InteractiveIsland/components/ProjectZotbot/ProjectZotbot.tsx
@@ -6,6 +6,8 @@ import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 import { memo, Suspense, useMemo } from 'react';
 import { getRandomSpawnState } from 'app/controller/random-gen-utils';
 import { RigidBody } from '@react-three/rapier';
+import { random } from 'app/utils/utils';
+import { Vector3 } from 'three';
 
 const ZOTBOT_GLTF = '/assets/delivery_robot/zotbot.gltf';
 
@@ -18,12 +20,13 @@ interface ProjectBallProps {
 export const ProjectZotbot = memo(function ProjectZotbot({ data, selected }: ProjectBallProps) {
     const { scene } = useLoader(GLTFLoader, ZOTBOT_GLTF);
     const { position, rotation, ref } = useMemo(() => getRandomSpawnState(), []);
+    const loweredAltitude = useMemo(() => new Vector3(position[0], random(20, 40), position[2]), [position])
 
     return (
         window.innerWidth > 768 && (
             <Suspense fallback={null}>
                 <RigidBody
-                    position={position}
+                    position={loweredAltitude}
                     rotation={rotation}
                     restitution={0}
                     colliders={'cuboid'}
